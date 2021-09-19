2fc0f43e921dceae63d24be03ff952b8c1036003
locriacyber <74560659+locriacyber@users.noreply.github.com>
2021-09-20 00:15:14 +0800

    Update preview.js + fix bugs

---
+++ src/markdown-it-plantuml/rule.ts
@@ -0,0 +1,29 @@
+import * as markdowIt from 'markdown-it';
+
+export function plantumlWorker(state: any) {
+    // debugInfo(state.tokens);
+    let blockTokens: markdowIt.Token[] = state.tokens;
+    for (let blockToken of blockTokens) {
+        if (blockToken.type == "fence" &&( blockToken.info.startsWith("plantuml")|| blockToken.info.startsWith("puml"))) {
+            blockToken.type = "plantuml";
+            // always render as <img> for maximum compatibility:
+            // https://github.com/qjebbs/vscode-markdown-extended/issues/67#issuecomment-554996262
+            blockToken.tag = "img";
+            // if (state.env && state.env.htmlExporter) { // work with markdown extended export, solve #253
+            //     blockToken.tag = "object";
+            // } else {
+            //     blockToken.tag = "img";
+            // }
+        }
+    }
+}
+
+function debugInfo(blockTokens: markdowIt.Token[]) {
+    for (let blockToken of blockTokens) {
+        console.log(blockToken.type, blockToken.info, blockToken.tag, blockToken.content);
+        if (!blockToken.children) continue;
+        for (let token of blockToken.children) {
+            console.log("children:", token.type, token.info, token.tag, token.content);
+        }
+    }
+}
